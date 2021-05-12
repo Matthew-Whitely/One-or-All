1891f45035ecda92d13c75ca0e14400faa945722
holjas <holly.jasiura@gmail.com>
2021-05-11 21:59:28 -0400

    maybe you already figured these out, but here's what I had done

--- src/Extras.js
+++ src/Extras.js
@@ -1,17 +1,46 @@
+import { useState, useEffect, useRef } from "react";
+
 function Extras() {
+  const [dice, setDice] = useState("");
+
   const diceRoll = () => {
     const randomNumber = Math.floor(Math.random() * 7);
     if (randomNumber <= 5) {
-      return "Name 1";
+      setDice("Name 1 thing");
     } else {
-      return "Name All";
+      setDice("Name ALL things");
     }
   };
+  //
+  const [num, setNum] = useState(100);
+  const [pause, setPause] = useState(false);
 
+  let intervalRef = useRef();
+
+  const decreaseNum = () => setNum((prev) => prev - 1);
+
+  useEffect(() => {
+    setPause(false);
+    intervalRef.current = setInterval(decreaseNum, 1000);
+
+    return () => clearInterval(intervalRef.current);
+  }, []);
+
+  const handleClick = () => {
+    if (!pause) {
+      clearInterval(intervalRef.current);
+    } else {
+      intervalRef.current = setInterval(decreaseNum, 1000);
+    }
+    setPause((prev) => !prev);
+  };
   return (
     <>
-      <p>Dice Roll = {diceRoll()}</p>
-      <p></p>
+      <button onClick={diceRoll}>DICE ROLL</button>
+      <p>{dice}</p>
+      {/*  */}
+      <div>{num}</div>
+      <button onClick={handleClick}>{pause ? "Run" : "Pause"}</button>
     </>
   );
 }
